353dac0760dfa6d8dbe93b473676632569351a36
harode12 <harode.pratik@gmail.com>
2025-10-28 15:25:54 +0530

    Update AlphanumericCheck.java

--- Day7/StringAlphaNumCheck/src/AlphanumericCheck.java
+++ Day7/StringAlphaNumCheck/src/AlphanumericCheck.java
@@ -7,9 +7,9 @@ public class AlphanumericCheck {
         String input = sc.nextLine();
 
         if (isAlphanumeric(input)) {
-            System.out.println("✅ The string contains only alphanumeric characters.");
+            System.out.println(" The string contains only alphanumeric characters.");
         } else {
-            System.out.println("❌ The string contains special characters or spaces.");
+            System.out.println(" The string contains special characters or spaces.");
         }
 
         sc.close();
